7370fcfb86b579492b002ea030a90eeff061c40f
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2020-01-27 16:04:11 -0500

    Dealing starting hands to players TODO: Game progression

--- deck.hs
+++ deck.hs
@@ -6,12 +6,21 @@ type Value = Int
 type Suit = Char
 type Card = (Value, Suit)
 type Deck = [Card]
+type Hand = (Int, [Card])
+type Board = [Card]
 
 deck :: Deck
 deck = [(x,y) | y <- "HSDC", x <- [2..14]]
 
 shuffleDeck :: Deck -> IO Deck
 shuffleDeck deck = knuthShuffle deck
+
+transcribeCard :: Card -> String
+transcribeCard (14,s) = 'A' : [s]
+transcribeCard (13,s) = 'K' : [s]
+transcribeCard (12,s) = 'Q' : [s]
+transcribeCard (11,s) = 'J' : [s]
+transcribeCard (v,s) = (show v) ++ [s]
 
 
 
--- main.hs
+++ main.hs
@@ -11,4 +11,24 @@ main = do
 
 
 playGame :: Int -> IO ()
-playGame numPlayers = putStrLn $ "Oh okay " ++ show numPlayers ++ " players"
+playGame numPlayers = do
+    putStrLn $ "Oh okay " ++ show numPlayers ++ " players"
+    sdeck <- shuffleDeck deck
+    let (hands, board, ssdeck) = deal sdeck numPlayers
+    putStrLn $ "Player hands: " ++ (show $ fmap (mapTuple (fmap transcribeCard)) hands)
+    putStrLn $ "Board: " ++ (show board)
+    putStrLn $ "Deck: " ++ (show $ fmap transcribeCard ssdeck)
+
+--startGame :: Deck -> Int -> IO ()
+
+deal :: Deck -> Int -> ([Hand], Board, Deck)
+deal deck n = let (x, d) = splitAt (2*n) deck
+              in (zip [1..n] $ chunksOf 2 x, [], d)
+
+
+chunksOf :: Int -> [Card] -> [[Card]]
+chunksOf _ [] = []
+chunksOf n xs = take n xs:chunksOf n (drop n xs)
+
+mapTuple :: (a -> b) -> (c, a) -> (c, b)
+mapTuple f (a1, a2) = (a1, f a2)
